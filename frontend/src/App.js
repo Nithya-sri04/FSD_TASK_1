1f05a67bbe24a5dc463612911b2de439da53a18d
Nithya-sri04 <nithyasrisivakumar30@gmail.com>
2024-12-19 18:02:07 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -52,7 +52,7 @@ const EmployeeForm = () => {
     if (!validate()) return; // Stop form submission if validation fails
 
     try {
-      await axios.post('https://emp-mgmt-okbt.onrender.com/api/add', formData);
+      await axios.post('https://fsd-task-1-2.onrender.com/api/add', formData);
       setSuccessMessage(response.data.message || 'Employee added successfully!');
       setErrorMessage('');
       setFormData({
